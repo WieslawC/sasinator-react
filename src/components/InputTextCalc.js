22dbd8a40f389546c0ff73ba4d04d56747f65235
Wiesław Bikowski <wieslaw.botak@gmail.com>
2021-04-04 20:32:36 +0200

    Improved input css

--- src/components/InputTextCalc.js
+++ src/components/InputTextCalc.js
@@ -1,18 +1,29 @@
+import { Component } from "react";
 import "../style/InputTextCalc.scss";
 
-const InputTextCalc = (props) => {
-  return (
-    <>
-      <input
-        type="number"
-        name="inputValue"
-        className="inputText"
-        value={props.value}
-        onChange={props.handleFunction}
-      />
-      <p className="displayText">{(props.value / 70000000).toFixed(8)} sasin</p>
-    </>
-  );
-};
+class InputTextCalc extends Component {
+  componentDidMount() {
+    document.querySelector(".inputText").addEventListener("click", () => {
+      document.querySelector(".inputText").classList.add("active");
+    });
+  }
+  render() {
+    return (
+      <>
+        <input
+          type="number"
+          name="inputValue"
+          className="inputText"
+          placeholder="---"
+          value={this.props.value}
+          onChange={this.props.handleFunction}
+        />
+        <p className="displayText">
+          {(this.props.value / 70000000).toFixed(8)} sasin
+        </p>
+      </>
+    );
+  }
+}
 
 export default InputTextCalc;
